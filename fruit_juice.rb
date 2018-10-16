875d00f424e9200ff3720a5d6fc48813d9f8e5d8
Danylo Denysov <danyldezwaan@hotmail.com>
2018-10-17 09:16:25 +1000

    hey

--- fruit_juice.rb
+++ fruit_juice.rb
@@ -12,7 +12,7 @@
 # Now you add 200 ml of apple juice
 # And then you add 200 ml of banana juice
 # Now the concentration of apple juice is 0.5 (50%)
-# Then you pour out 200 ml
+# Then you pour out 200 ml29_fruit_juice
 # The concentration of apple juice is still 50%
 # Then you add 200 ml of apple juice again
 # Now the concentration of apple juice is 0.75, while the concentration of banana juice
@@ -21,18 +21,38 @@
 # Complete the functions in order to provide this functionality.
 
 class Jug
+    
     def initialize(capacity)
+        @capacity = capacity
+        @concentration = {}
+        @amount = 0
+    end
+
+    def calc_total_amount
+        tem_value =
+        for key, value in @concentration do
+            tem_value += value
+        end
+
+        @amount = tem_value
     end
 
     def add(amount, type)
+        @concentration['apple'] = amount
+        @amount += amount
+        @amount = @capacity if @amount > @capacity
     end
 
     def pour(amount)
+        @amount -= amount
     end
 
     def getTotalAmount
+        return @amount
     end
 
     def getConcentration(type)
+        return 0 if @concentration[type] == nil
+        return @concentration[type] / @amount.to_f
     end
 end
