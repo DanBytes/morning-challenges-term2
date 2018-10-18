2a2e41b48e571936e616f6f36e85d05ff02bffb0
Danylo Denysov <danyldezwaan@hotmail.com>
2018-10-19 09:38:16 +1000

    Finished challange

--- extend_array.rb
+++ extend_array.rb
@@ -1,7 +1,25 @@
 class Array
   def square
     # create your square method here
+    map {|x| x ** 2}
   end
-
-  # now fill in the rest
+  
+  def cube
+    map {|x| x ** 3}
+  end
+  
+  def average
+    return sum / length
+  end
+  
+  def even
+   select {|num| num.even?}
+  end
+  
+  def odd
+    select {|num|  !num.even?}
+  end
 end
+
+# now fill in the rest
+
