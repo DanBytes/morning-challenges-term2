83d19d8c6b06581e19226486acd6afbedc399200
Danylo Denysov <danyldezwaan@hotmail.com>
2018-10-22 08:42:18 +1000

    morning challange

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -23,8 +23,22 @@
 #   M	    1000
 #
 class RomanNumerals
+    def initialize 
+        @numbers = {
+            "I" => 1,
+            "V" => 5,
+            "X" => 10,
+            "L" => 50,
+            "C" => 100,
+            "D" => 500,
+            "M" => 1000
+        }
+    end
+
     def self.fromRoman(number)
+
     end
     def self.toRoman(number)
+        
     end
 end
