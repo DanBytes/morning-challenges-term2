dca079592f549a38b4192dba75100a5c30470130
Danylo Denysov <danyldezwaan@hotmail.com>
2018-11-14 09:30:58 +1000

    completed the challange

--- 03_open_door.js
+++ 03_open_door.js
@@ -8,7 +8,13 @@ mocha 03_open_door.js
 
 function openDoor(input) {
     // Your code here
-
+    for (item of input) {
+        console.log(input);
+        if (item.name === "Simon" && item.hasCoffee === true) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function makeItem(name, hasCoffee) {
@@ -27,19 +33,23 @@ describe('openDoor', function () {
         assert.equal(true, openDoor(hash))
     })
     it('should return true if "Simon" and hasCoffee with multiple objects', function () {
+        let hash = [];
         hash.push(makeItem('Simon', true))
         hash.push(makeItem('Scott', true))
         assert.equal(true, openDoor(hash))
     })
     it('should return false if !"Simon" and hasCoffee', function () {
+        let hash = [];
         hash.push(makeItem('Scott', true))
         assert.equal(false, openDoor(hash))
     })
     it('should return false if "Simon" and !hasCoffee', function () {
+        let hash = [];
         hash.push(makeItem('Simon', false))
         assert.equal(false, openDoor(hash))
     })
     it('should return false if empty array passed', function () {
+        let hash = [];
         assert.equal(false, openDoor(hash))
     })
 })
