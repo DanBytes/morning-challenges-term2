cc27fbb12bd2160846ae504f4f417a6e74b7177d
Danylo Denysov <danyldezwaan@hotmail.com>
2018-11-14 08:54:49 +1000

    merging

--- coolstrings/01_coolstrings.js
+++ coolstrings/01_coolstrings.js
@@ -1,39 +1,41 @@
 // Create a new variable called coolstrings which cannot be reassigned
 // and set its value to 'coolstrings'
-
+const coolstrings = "coolstrings"
 // Create a second variable called numbers which can be reassigned
 // and set its value to 7
-
+var number = 7
 // Instead of using:
-  // def()
-  // 
-  // end
+// def()
+// 
+// end
 // What is the main way to write methods in JavaScript?
+function myFunction() {
 
+}
 // Instead of puts, what does JavaScript use instead?
-
+console.log(number)
 
 
 
 
 function setNumbers(num) {
-  numbers = num;  
+  numbers = num;
 }
 var assert = require('assert');
-  
+
 describe('coolstrings', () => {
-    it('checks that coolstrings cannot be changed', function() {
-      try {
-        coolstrings = 'abc';
-      } catch (err) {
-        //do nothing
-      }
-      assert.equal(coolstrings,"coolstrings");
-    });
+  it('checks that coolstrings cannot be changed', function () {
+    try {
+      coolstrings = 'abc';
+    } catch (err) {
+      //do nothing
+    }
+    assert.equal(coolstrings, "coolstrings");
+  });
 });
 
 describe('numbers function', () => {
-  it('checks that numbers can be changed', function() {
+  it('checks that numbers can be changed', function () {
     setNumbers(9);
     assert.equal(numbers, 9);
   });
--- forum/config/routes.rb
+++ forum/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'questions/create'
+  resources :answers
+  resources :questions
   # Go to home/index by default
   root 'home#index'
   # About page
   get '/about' => 'home#about'
 
-  post '/questions' => 'home#temp'
+   post '/questions' => 'questions#index'
   post '/answers' => 'home#temp'
 
-  get '/questions/:id' => 'home#question'
+  # get '/questions/:id' => 'home#question'
 end
