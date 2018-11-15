5306435e6c1340e0e8337d23379865e6fdb62b25
Danylo Denysov <danyldezwaan@hotmail.com>
2018-11-15 10:28:37 +1000

    changes

--- 03_open_door.js
+++ 03_open_door.js
@@ -15,41 +15,40 @@ function openDoor(input) {
         }
     }
     return false;
-}
 
-function makeItem(name, hasCoffee) {
-    item = new Object()
-    item.name = name
-    item.hasCoffee = hasCoffee
-    return item
-}
+    function makeItem(name, hasCoffee) {
+        item = new Object()
+        item.name = name
+        item.hasCoffee = hasCoffee
+        return item
+    }
 
-var assert = require('assert');
+    var assert = require('assert');
 
-describe('openDoor', function () {
-    let hash = [];
-    it('should return true if "Simon" and hasCoffee with single object', function () {
-        hash.push(makeItem('Simon', true))
-        assert.equal(true, openDoor(hash))
-    })
-    it('should return true if "Simon" and hasCoffee with multiple objects', function () {
+    describe('openDoor', function () {
         let hash = [];
-        hash.push(makeItem('Simon', true))
-        hash.push(makeItem('Scott', true))
-        assert.equal(true, openDoor(hash))
+        it('should return true if "Simon" and hasCoffee with single object', function () {
+            hash.push(makeItem('Simon', true))
+            assert.equal(true, openDoor(hash))
+        })
+        it('should return true if "Simon" and hasCoffee with multiple objects', function () {
+            let hash = [];
+            hash.push(makeItem('Simon', true))
+            hash.push(makeItem('Scott', true))
+            assert.equal(true, openDoor(hash))
+        })
+        it('should return false if !"Simon" and hasCoffee', function () {
+            let hash = [];
+            hash.push(makeItem('Scott', true))
+            assert.equal(false, openDoor(hash))
+        })
+        it('should return false if "Simon" and !hasCoffee', function () {
+            let hash = [];
+            hash.push(makeItem('Simon', false))
+            assert.equal(false, openDoor(hash))
+        })
+        it('should return false if empty array passed', function () {
+            let hash = [];
+            assert.equal(false, openDoor(hash))
+        })
     })
-    it('should return false if !"Simon" and hasCoffee', function () {
-        let hash = [];
-        hash.push(makeItem('Scott', true))
-        assert.equal(false, openDoor(hash))
-    })
-    it('should return false if "Simon" and !hasCoffee', function () {
-        let hash = [];
-        hash.push(makeItem('Simon', false))
-        assert.equal(false, openDoor(hash))
-    })
-    it('should return false if empty array passed', function () {
-        let hash = [];
-        assert.equal(false, openDoor(hash))
-    })
-})
